e242eae6fd162fcf5ecbd81dd2aadc6716336d46
Yago Pacheco <49993715+YagoPacheco@users.noreply.github.com>
2025-03-18 18:34:31 -0300

    Update AppContext.tsx

--- src/components/AppContext.tsx
+++ src/components/AppContext.tsx
@@ -18,7 +18,7 @@ export const AppContextProvider = ({ children }: any) => {
       const { login } = JSON.parse(storage)
       setIsLoggedIn(login);
     }
-  }, [setIsLoggedIn, true]));
+  }, [storage]));
 
   return (
     <AppContext.Provider value={{ isLoggedIn, setIsLoggedIn }}>
