fcb0dd27f873c38f87f94f17dab14126080b4073
YagoPacheco <yagopacheco4@gmail.com>
2025-03-18 17:27:41 -0300

    modified:   src/components/Header.tsx 	modified:   src/styles/Main.css

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -21,7 +21,7 @@ export const Header = () => {
         <Box className='banner'>
           <Center>
             <img alt='Digital Innovation One logo' src='https://ac-landing-pages-user-uploads-production.s3.amazonaws.com/0000051657/74fb05a0-78a9-423e-ac00-55c228f48c89.png'></img>
-            <text>Dio Bank</text>
+            <text>Bank</text>
           </Center>
         </Box>
         {
